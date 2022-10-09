98f810375d6b0efe943437b4e3deb1bc4ed314fc
sejapoe <bekaldiev2015@gmail.com>
2022-10-09 14:33:27 +0300

    add pawn attack style

--- app/src/main/java/com/sejapoe/chess/game/piece/Pawn.kt
+++ app/src/main/java/com/sejapoe/chess/game/piece/Pawn.kt
@@ -11,7 +11,9 @@ class Pawn(override val color: PieceColor, override val imageResource: Int, priv
             PieceColor.WHITE -> 1
         }
         if (r + mul !in 0..7) return
-        board.cells[r + mul][c].setCellState(CellState.MOVE)
+        if (board.cells[r + mul][c].piece == null) board.cells[r + mul][c].setCellState(CellState.MOVE)
+        if (c != 0 && board.cells[r + mul][c - 1].piece != null) board.cells[r + mul][c - 1].setCellState(CellState.ATTACK)
+        if (c != 7 && board.cells[r + mul][c + 1].piece != null) board.cells[r + mul][c + 1].setCellState(CellState.ATTACK)
         if (r == startRow) board.cells[r + mul * 2][c].setCellState(CellState.MOVE)
     }
 }
